58936b4a376c19afb32b62d5db5607650d6a3b6f
numeru <cyk000418@naver.com>
2021-02-27 22:42:44 +0900

    add product upload page

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -10,7 +10,7 @@ function FileUpload(props) {
   const dropHandler = (files) => {
     let formData = new FormData();
     const config = {
-      header: { "content-type": "multipart/fomr-data" },
+      header: { "content-type": "multipart/form-data" },
     };
     formData.append("file", files[0]);
 
